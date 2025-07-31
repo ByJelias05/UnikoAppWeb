b02650a31dbe31930144f64659f99955e09235bf
ByJelias05 <garciassierra@hotmail.com>
2025-07-30 22:17:02 -0400

    Autenticacion com google agregado

--- Client/src/Views/Login.jsx
+++ Client/src/Views/Login.jsx
@@ -7,6 +7,8 @@ import {LoginContext} from "../Routers/Routes"
 
 import {Cookies} from "react-cookie"
 
+import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 export function Login(){
 
     const [Usuario, setUsuario] = useContext(LoginContext)
@@ -40,6 +42,33 @@ export function Login(){
                         
         })
         
+    }
+    
+    const provider = new GoogleAuthProvider();
+
+        const Google = () =>{
+            const auth = getAuth();
+
+            signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user)
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+        });
     }
 
     return(
@@ -81,7 +110,7 @@ export function Login(){
                 </div>
 
                 <div className="Otros-Metodos">
-                    <div className="Box-Metodo"></div>
+                    <div className="Box-Metodo" onClick={Google}></div>
                     <div className="Box-Metodo"></div>
                     <div className="Box-Metodo"></div>
                 </div>
